695aa14333f713892aab147875a8f41a1edb2ba9
bpmaurya <realvedprakash@gmail.com>
2021-03-07 11:57:31 +0530

    add functions

--- core/serializers.py
+++ core/serializers.py
@@ -22,6 +22,25 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
 
+    def update(self,instance,validate_data):
+        try:
+            user = instance
+            password= validate_data.pop('password')
+            old_password = validate_data.pop('old_password')
+            if user.check_password(old_password):
+                user.set_password(password)
+            else:
+                raise Exception("Old password is Incorrect")
+            user.save()
+
+        except Exception as err:
+            raise serializers.ValidationError(err)
+
+        return super(UserSerializer,self).update(instance,validated_data)
+
+
+
+
     class Meta:
         model = User 
         fields=['url','id','username','email','first_name','last_name','password']
